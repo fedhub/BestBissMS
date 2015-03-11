07e127b90ed477612af5b7f099dcb9031942fd46
Shimon <shimi0007@hotmail.com>
2015-03-11 13:35:23 +0200

    WIP Port fix

--- server/server.js
+++ server/server.js
@@ -33,8 +33,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(require('./routers'));
 
 // start the server
-app.listen(3000, function(){
-	console.log("app ready on port 3000");
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function(){
+	console.log("app ready on port "+port);
 });
 
 
