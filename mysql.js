a9846dad0389470ce2e7a8ca5db1a6d50a325576
Shimon <shimi0007@hotmail.com>
2015-03-20 17:55:01 +0200

    WIP main functionality

--- mysql.js
+++ mysql.js
@@ -3,12 +3,19 @@ var my_sql     = require('mysql');
 var mysql      = express.Router();
 
 mysql.MySql_Connection = my_sql.createConnection({
-    host     : '127.0.0.1',
-    user     : 'root',
-    port	 : 3306,
-    //password : 'shenkarYoker5',
-    database : 'best_biss'
+    host     : 'mysql17.000webhost.com',
+    user     : 'a9396256_sw',
+    password : 'shenkarYoker5',
+    database : 'a9396256_best'
 });
+
+//mysql.MySql_Connection = my_sql.createConnection({
+//    host     : '127.0.0.1',
+//    user     : 'root',
+//    port	 : 3306,
+//    //password : 'shenkarYoker5',
+//    database : 'best_biss'
+//});
 
 mysql.MySql_Connection.connect(function(err){
     if(!err) {
