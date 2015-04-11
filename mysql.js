4fc757bad59d949e558ad6a2bd68d12ae1534f20
Shimon <shimi0007@hotmail.com>
2015-04-11 17:43:55 +0300

    WIP ordering system

--- mysql.js
+++ mysql.js
@@ -2,12 +2,20 @@ var express    = require('express');
 var my_sql     = require('mysql');
 var mysql      = express.Router();
 
+/*mysql.MySql_Connection = my_sql.createConnection({
+    host     : 'mysql17.000webhost.com',
+    user     : 'a9396256_sw',
+    //password : 'shenkarYoker5',
+    database : 'a9396256_best'
+    //port     : port2
+});*/
+
 mysql.MySql_Connection = my_sql.createConnection({
-    host     : 'mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/',
-    user     : 'adminjecMATf',
+    host     : 'us-cdbr-azure-southcentral-e.cloudapp.net',
+    user     : 'bbedb78c1fea39',
     //port	 : 3306,
-    password : 'z6vtVT6CwJnx',
-    database : 'nodejs'
+    password : '54c23032',
+    database : 'bestbisAflwSbD71'
 });
 
 mysql.MySql_Connection.connect(function(err){
