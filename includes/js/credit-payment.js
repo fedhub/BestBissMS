b988fdf32061e683a3a06a3aef86a708a7fb4390
shimonsw <shimi0007@hotmail.com>
2015-04-18 16:58:11 +0300

    WIP - Returning from payment

--- includes/js/credit-payment.js
+++ includes/js/credit-payment.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     get_logo();
 
     $('.back-button').click(function(){
-        window.parent.history.back(-2);
+        parent.location.href = "https://www.best-biss.com/payment-success";
     });
 
 });
